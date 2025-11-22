073dc00a99e9d61b7a9fcd2b29acc97939011f70
Ernest Ibeh <ibeh_ernest@yahoo.com>
2025-11-22 15:40:04 +0100

    Added api fetch

---
+++ components/MovieCard.tsx
@@ -0,0 +1,49 @@
+import { icons } from "@/constants/icons";
+import { Movie } from "@/interface/movie";
+import { Link } from "expo-router";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+interface MovieCardProps {
+  movie: Movie;
+}
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+  console.log(movie.poster_path);
+  return (
+    <Link href={`/movies/${movie.id}`} asChild>
+      <TouchableOpacity className="w-[30%]">
+        <Image
+          source={{
+            uri: movie.poster_path
+              ? `https://media.themoviedb.org/t/p/w500${movie.poster_path}`
+              : "https://placehold.co/600x400/1a1a1a/ffffff.png",
+          }}
+          className="w-full h-52 rounded-lg"
+          resizeMode="cover"
+        />
+
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {movie.title}
+        </Text>
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs font-bold text-white uppercase">
+            {Math.round(movie.vote_average / 2)}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {movie.release_date?.split("-")[0]}
+          </Text>
+          <Text className="text-xs text-light-300 font-medium uppercase">
+            Movie
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+export default MovieCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
-        primary: "#14B8A6",
+        primary: "#030014",
         secondary: "#151312",
         light: {
           100: "#D6C6FF",
